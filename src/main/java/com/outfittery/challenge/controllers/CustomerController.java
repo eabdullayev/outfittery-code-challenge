dea6a48536a4420fc43d0da0b16aa6ba3460ca66
elchin.abdullayev <elchin.abdullayev@accenture.com>
2018-08-07 18:52:51 +0300

    added input vaildation for CustomerController

--- src/main/java/com/outfittery/challenge/controllers/CustomerController.java
+++ src/main/java/com/outfittery/challenge/controllers/CustomerController.java
@@ -7,10 +7,14 @@ import com.outfittery.challenge.rest.dto.CustomerResponse;
 import com.outfittery.challenge.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("api/v1/customer")
 public class CustomerController {
@@ -20,29 +24,28 @@ public class CustomerController {
     @GetMapping("/all")
     @ResponseStatus(HttpStatus.OK)
     public List<CustomerResponse> getAll() {
-        //todo implement validation
         return customerService.getAll();
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public CustomerResponse getById(@PathVariable(name = "id") Long id) {
-        //todo implement validation
+    public CustomerResponse getById(@Positive(message = "should be positive non zero value")
+                                    @PathVariable(name = "id")
+                                    Long id) {
         return customerService.getById(id);
     }
 
     @PostMapping("/add")
     @ResponseStatus(HttpStatus.CREATED)
-    public CustomerResponse createCustomer(@RequestBody Customer customer) {
-        //todo implement validation
+    public CustomerResponse createCustomer(@Valid @RequestBody Customer customer) {
         return customerService.save(customer);
     }
 
     @DeleteMapping("/remove/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public String deleteCustomer(@PathVariable(name = "id") Long id) {
-        //todo implement validation and return type
+    public void deleteCustomer(@Positive(message = "should be positive non zero value")
+                                 @PathVariable(name = "id")
+                                 Long id) {
         customerService.delete(id);
-        return "ok";
     }
 }
--- src/main/java/com/outfittery/challenge/models/Customer.java
+++ src/main/java/com/outfittery/challenge/models/Customer.java
@@ -1,6 +1,8 @@
 package com.outfittery.challenge.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
@@ -10,6 +12,8 @@ public class Customer {
     @SequenceGenerator(name = "SEQ_CUSTOMER_ID", sequenceName = "SEQ_CUSTOMER_ID", initialValue = 1, allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_CUSTOMER_ID")
     private Long id;
+    @NotBlank(message = "Name can not be blank")
+    @Size(message = "name length should be min 2 and max 50", min = 2, max = 50)
     private String name;
 
     @OneToMany(mappedBy = "customer")
